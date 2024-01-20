8b4ea7a6f426189b068a6bfd454026081d95fa35
Ignacio <Isaavedrapimentel@gmail.com>
2024-01-19 23:23:03 -0300

    cambios para despliege en vercel

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,36 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { ChakraProvider } from '@chakra-ui/react'
+
+
+// import './index.css';
+
+// import { router } from "./router";
+// import { RouterProvider } from "react-router-dom";
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//           <ChakraProvider>
+//                 <RouterProvider router={router} />
+//           </ChakraProvider>
+//   </React.StrictMode>
+// );
+
+
+
+// opcion gpt
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ChakraProvider } from '@chakra-ui/react'
-
+import { ChakraProvider } from '@chakra-ui/react';
+import AppRouter from './router/index';
 
 import './index.css';
 
-import { router } from "./router";
-import { RouterProvider } from "react-router-dom";
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-          <ChakraProvider>
-                <RouterProvider router={router} />
-          </ChakraProvider>
+    <ChakraProvider>
+      <AppRouter />
+    </ChakraProvider>
   </React.StrictMode>
 );
-
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,24 +1,51 @@
-import { createBrowserRouter } from "react-router-dom";
-import Home from "../pages/Home";
-import Camara from "../pages/Camara";
-import Alarma from "../pages/Alarma";
-import Blog from "../pages/Blog";
+// import { createBrowserRouter } from "react-router-dom";
+// import Home from "../pages/Home";
+// import Camara from "../pages/Camara";
+// import Alarma from "../pages/Alarma";
+// import Blog from "../pages/Blog";
 
-export const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Home />,
-    },
-    {
-        path: "/camara",
-        element: <Camara />,
-    },
-    {
-        path: "/alarma",
-        element: <Alarma />,
-    },
-    {
-        path: "/blog",
-        element: <Blog />,
-    },
-]);
+// export const router = createBrowserRouter([
+//     {
+//         path: "/",
+//         element: <Home />,
+//     },
+//     {
+//         path: "/camara",
+//         element: <Camara />,
+//     },
+//     {
+//         path: "/alarma",
+//         element: <Alarma />,
+//     },
+//     {
+//         path: "/blog",
+//         element: <Blog />,
+//     },
+// ]);
+
+//opcion 2 gpt
+
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from '../pages/Home';
+import Camara from '../pages/Camara';
+import Alarma from '../pages/Alarma';
+import Blog from '../pages/Blog';
+
+const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/camara" element={<Camara />} />
+        <Route path="/alarma" element={<Alarma />} />
+        <Route path="/blog" element={<Blog />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AppRouter;
+
+
+
